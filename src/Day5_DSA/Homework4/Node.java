6cc5a6e0df757e9861df7ac7f4a6c45901154767
khangvo95 <khang.vo@getoutabox.com>
2026-01-21 16:01:03 +0700

    Queue

---
+++ src/Day5_DSA/Homework4/Node.java
@@ -0,0 +1,11 @@
+package Day5_DSA.Homework4;
+
+public class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+}

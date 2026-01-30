8944a09669cb351a602a25e523e2b974841be185
khangvo95 <khang.vo@getoutabox.com>
2026-01-30 10:38:26 +0700

    NumberFormatException

---
+++ src/Exception/CalculationExample.java
@@ -0,0 +1,32 @@
+package Exception;
+
+import java.util.Scanner;
+
+public class CalculationExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hãy nhập x: ");
+        int x = sc.nextInt();
+
+        System.out.println("Hãy nhập y: ");
+        int y = sc.nextInt();
+
+        CalculationExample  c = new CalculationExample();
+        c.calculate(x,y);
+    }
+
+    private void calculate(int x, int y){
+        try {
+            int a = x + y;
+            int b = x - y;
+            int c = x * y;
+            int d = x / y;
+            System.out.println("x + y = " + a);
+            System.out.println("x - y = " + b);
+            System.out.println("x * y = " + c);
+            System.out.println("x / y = " + d);
+        } catch (Exception e) {
+            System.err.println("Xảy ra ngoại lệ: " + e.getMessage());
+        }
+    }
+}
